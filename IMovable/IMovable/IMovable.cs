4a485c1c5dd22aa84582d81341a36f6a745343b5
snowRepo <efarthur002@st.ug.edu.gh>
2024-07-02 18:30:22 +0000

    Completed IMovable Program

---
+++ IMovable/IMovable/IMovable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMovables
+{
+     interface IMovable
+    {
+        void Move();
+    }
+}
---
+++ IMovable/IMovable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using IMovables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMovable car = new Car_Class();
+        IMovable bicycle = new Bicycle_Class();
+
+
+        car.Move();
+        bicycle.Move();
+    }
+}
