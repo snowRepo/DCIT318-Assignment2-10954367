1fb4c950329d8036fec1afd602c66841028d3ee8
snowRepo <efarthur002@st.ug.edu.gh>
2024-07-02 14:58:08 +0000

    Completed Animal Program

---
+++ Animal/Animal/Base Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal 
+{
+    internal class Base_Class
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+        
+        
+    }
+}
---
+++ Animal/Animal/Program.cs
@@ -0,0 +1,14 @@
+﻿using Animal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Base_Class myDog = new Class_Dog();
+        Base_Class myCat = new Class_Cat();
+
+
+        myDog.MakeSound();
+        myCat.MakeSound();
+    }
+}
